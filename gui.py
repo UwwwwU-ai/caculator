724ddf7a1c990d8a54c1e06058608c83660ea0f6
UwwwwU-ai <nobody05@qq.com>
2026-09-16 16:17:45 +0800

    2026/9/16/16:17

--- gui.py
+++ gui.py
@@ -1,1 +1,8 @@
 import tkinter as tk
+from tkinter import messagebox
+window = tk.Tk()
+window.title("Calculator")
+w, h = window.maxsize()
+window.geometry(f"360x560+{(w - 360) // 2}+{(h - 560) // 2}")
+window.resizable(False, False)
+window.configure(bg="#1e1e1e")
